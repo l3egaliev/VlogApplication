b3da89221e51f38c04000dab0f4cde49537a8324
Rocky <tedrocky@gmail.com>
2023-11-07 21:34:46 +0600

    Начинаю создать приложение влог

---
+++ src/test/java/kg/rakhim/classes/Vlog/VlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.rakhim.classes.Vlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
